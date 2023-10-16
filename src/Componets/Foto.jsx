24eaeb18633bb5f3cf07f53fbc0ab9e9267d4cc3
sebastian alvarez <alvarezpaezsebastian03@gmail.com>
2023-10-16 13:58:32 +0200

    siuu

--- src/Componets/Foto.jsx
+++ src/Componets/Foto.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Foto = ({ foto }) => {
   return (
     <div>
-      <img src={`src/assets/${foto}`} alt="" width="300px" />
+      <img src={`/${foto}`} alt="" width="300px" />
     </div>
   );
 };
