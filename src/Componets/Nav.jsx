4b79b725bc1f8e170486fe03afdd7b9fa405e1b9
sebastian alvarez <alvarezpaezsebastian03@gmail.com>
2023-10-16 14:10:26 +0200

    casi

--- src/Componets/Nav.jsx
+++ src/Componets/Nav.jsx
@@ -7,18 +7,22 @@ import { BsFillArrowThroughHeartFill } from "react-icons/bs";
 export const Nav = () => {
   return (
     <nav className="flex justify-between bg-indigo-300 p-4 rounded">
-      <div className="flex items-center gap-2">
-        <p>Sara Valentina Rodriguez Rodriguez</p>
-        <AiFillHeart />
-      </div>
+      <a href="https://instagram.com/__saraavl?igshid=MzRlODBiNWFlZA==">
+        <div className="flex items-center gap-2">
+          <p>Sara Valentina Rodriguez Rodriguez</p>
+          <AiFillHeart />
+        </div>
+      </a>
       <div className="flex item-center gap-2">
         <p>Lo que significas para mi</p>
         <BsFillArrowThroughHeartFill />
       </div>
-      <div className="flex items-center gap-2">
-        <p>Siendo tu</p>
-        <BsFillMoonStarsFill />
-      </div>
+      <a href="https://instagram.com/svrr08_?igshid=NzZlODBkYWE4Ng==">
+        <div className="flex items-center gap-2">
+          <p>Siendo tu</p>
+          <BsFillMoonStarsFill />
+        </div>
+      </a>
       <div className="flex items-center gap-2">
         <p>Nosotros</p>
         <div className="flex">
